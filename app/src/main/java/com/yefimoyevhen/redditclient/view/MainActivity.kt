36e2a706d4687ba6128250262414cc70e808f84f
Yevhen Yefimov <yefimovyevhen@gmail.com>
2021-07-26 21:13:01 +0300

    first commit

---
+++ app/src/main/java/com/yefimoyevhen/redditclient/view/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.yefimoyevhen.redditclient.view
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.yefimoyevhen.redditclient.R
+import com.yefimoyevhen.redditclient.databinding.ActivityMainBinding
+import com.yefimoyevhen.redditclient.viewModel.RedditViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    val viewModel: RedditViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFrag = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFrag.navController
+        setupActionBarWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.entriesFragment,
+                R.id.detailsEntryFragment
+            )
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.refresh_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.item_refresh) {
+            viewModel.fetchData()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
